8482482e9b6ac69dcb20cc1e7f53ca760748e913
Sławomir Siudek <siudeks@gmail.com>
2016-05-29 18:02:36 +0200

    Initial scaffolding

---
+++ TacticsAndStrategy/Client.Tests/UnitTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace Client.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
