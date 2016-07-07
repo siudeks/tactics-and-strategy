432188e8754a0183f2207ca86f4f4171cc9ca63d
Sławomir Siudek <siudeks@gmail.com>
2016-07-07 22:53:57 +0200

    Refactor.

--- src/Client.UWP/View/Window.cs
+++ src/Client.UWP/View/Window.cs
@@ -9,7 +9,7 @@ namespace Client.View
     /// </summary>
     public sealed class Window
     {
-        private readonly Dictionary<GeoPoint, TextureHolder> points = new Dictionary<GeoPoint, TextureHolder>();
+        private readonly Dictionary<GeoPoint, LocationType> points = new Dictionary<GeoPoint, LocationType>();
 
         private Dictionary<DirectionEnum, TextureHolder> water;
         private TextureHolder ground;
@@ -26,26 +26,45 @@ namespace Client.View
             foreach (var item in island.GeneratePoints())
             {
                 var point = new GeoPoint(item.X, item.Y);
-                points.Add(point, ground);
+                points.Add(point, LocationType.Ground);
             }
         }
 
         public void AddCity(CityEntity entity)
         {
             var point = new GeoPoint(entity.X, entity.Y);
-            points[point] = city;
+            points[point] = LocationType.City;
         }
 
+        /// <summary>
+        /// Returns Texture for given location.
+        /// </summary>
+        /// <param name="geox"></param>
+        /// <param name="geoy"></param>
+        /// <returns></returns>
         public TextureHolder this[int geox, int geoy]
         {
             get
             {
                 var index = new GeoPoint(geox, geoy);
                 if (points.ContainsKey(index))
-                    return points[index];
-                else
-                    return water[DirectionEnum.Unknown];
+                    switch (points[index])
+                    {
+                        case LocationType.City:
+                            return city;
+                        case LocationType.Ground:
+                            return ground;
+                    }
+
+                return water[DirectionEnum.Unknown];
             }
+        }
+
+        private enum LocationType
+        {
+            Water,
+            City,
+            Ground
         }
     }
 }
